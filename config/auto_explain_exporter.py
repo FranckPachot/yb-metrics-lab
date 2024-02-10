13b95118e6a4f89a6209508c16356a2ca1cfb4f1
Franck Pachot <mail@pachot.net>
2024-02-10 21:59:25 +0000

    recognizes quoted identifier

--- config/auto_explain_exporter.py
+++ config/auto_explain_exporter.py
@@ -14,13 +14,13 @@ def main():
 
 def gather_metrics():
     print("collecting httpd metrics")
-    regex = r"^[ \t->]*(.*[^ ]) *(\(cost=([0-9.]+)\.\.([0-9.]+) rows=([0-9]+) width=([0-9]+)\) )\(actual time=([0-9.]+)\.\.([0-9.]+) rows=([0-9]+) loops=([0-9]+)\).*"
+    regex = r'^\t[ ->]*(.*) on ([^ "]+|"[^"]+").* (\(cost=([0-9.]+)\.\.([0-9.]+) rows=([0-9]+) width=([0-9]+)\) )\(actual time=([0-9.]+)\.\.([0-9.]+) rows=([0-9]+) loops=([0-9]+)\).*'
     explain_node = Gauge('explain_node_total_rows', 'auto_explain rows*loop', ["node"])
     for line in follow_log( sys.argv[1] ):
         match = re.match(regex, line)
         if match:
             print(f"node: {line}")
-            explain_node.labels(node=match.group(1)).inc( (int(match.group(9)) * int(match.group(10)) ) )
+            explain_node.labels(node= (match.group(1)+" on "+match.group(2)) ).inc( (int(match.group(10)) * int(match.group(11)) ) )
 
 
 def follow_log(file):
